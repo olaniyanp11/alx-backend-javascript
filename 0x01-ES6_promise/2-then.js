011c2031aceaa6cef01bea78daab3c8a1a3a84f3
olaniyanp11 <olaniyanp11@gmail.com>
2023-07-27 10:37:36 +0100

    task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log("Got a response from the API");
+      return {
+        status: 200,
+        body: "success",
+      };
+    })
+    .catch((Error) => {
+      console.log("Got a response from the API");
+      return new Error();
+    });
+}
+
+
