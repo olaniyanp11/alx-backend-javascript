e36f108a2f109c9ee095c3b368eb578fb0373f0e
olaniyanp11 <olaniyanp11@gmail.com>
2023-07-27 09:16:08 +0100

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
 export default function getResponseFromAPI() {
-  return Promise.resolve("true");
+    return new Promise((resolve, reject) => {
+        resolve(() => {
+            console.log("true");
+        })
+    })
 }
