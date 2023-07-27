753aa18fbbb6850b8108c6f2c77b3cf9069e930b
olaniyanp11 <olaniyanp11@gmail.com>
2023-07-27 14:47:34 +0100

    task-4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    resolve({
-      firstname: firstName,
-      lastname: lastName,
-    });
+  return Promise.resolve({
+    firstName,
+    lastName,
   });
 }
