16d10acb8b1fa6c69d8bef493c7eccec2e41a084
olaniyanp11 <olaniyanp11@gmail.com>
2023-07-28 02:46:32 +0100

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+import Building from "./5-building";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.__floors = floors;
+    }
+    get floors() {
+        return this.__floors
+    }
+    set floors(value) {
+        this.__floors = value
+    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.__floors} floors`;
+  }
+}
