613934d925f1831b7cfec44265da2683e22c9490
olaniyanp11 <olaniyanp11@gmail.com>
2023-09-21 07:39:11 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
   if (name !== null) {
@@ -8,6 +8,6 @@ process.stdin.on("readable", () => {
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
